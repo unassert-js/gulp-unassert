8d25e4f0cf99d11242febe0dff0ea5db322544b2
Takuto Wada <takuto.wada@gmail.com>
2018-06-12 10:47:32 +0900

    fix: replace deprecated gulp-util with plugin-error

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
 
 var unassert = require('unassert');
 var through = require('through2');
-var gutil = require('gulp-util');
+var PluginError = require('plugin-error');
 var bufferFrom = require('buffer-from');
 var BufferStreams = require('bufferstreams');
 var acorn = require('acorn');
@@ -93,18 +93,18 @@ module.exports = function (opt) {
                 transform(file, encoding, opt);
                 this.push(file);
             } catch (err) {
-                return callback(new gutil.PluginError('gulp-unassert', err, {showStack: true}));
+                return callback(new PluginError('gulp-unassert', err, {showStack: true}));
             }
         } else if (file.isStream()) {
             file.contents = file.contents.pipe(new BufferStreams(function(err, buf, cb) {
                 if(err) {
-                    cb(new gutil.PluginError('gulp-unassert', err, {showStack: true}));
+                    cb(new PluginError('gulp-unassert', err, {showStack: true}));
                 } else {
                     try {
                         var modifiedCode = applyUnassertWithoutSourceMap(buf.toString(encoding));
                         cb(null, bufferFrom(modifiedCode));
                     } catch (innerError) {
-                        return callback(new gutil.PluginError('gulp-unassert', innerError, {showStack: true}));
+                        return callback(new PluginError('gulp-unassert', innerError, {showStack: true}));
                     }
                 }
             }));
