8ea089e451c8d1f10a5c4726b3b6a73ad73abfef
Takuto Wada <takuto.wada@gmail.com>
2020-05-12 21:52:27 +0900

    style: apply semistandard

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * gulp-unassert
  *   Gulp plugin for unassert
  *     Encourages programming with assertions by providing tools to compile them away.
- * 
+ *
  * https://github.com/unassert-js/gulp-unassert
  *
  * Copyright (c) 2015-2018 Takuto Wada
@@ -22,94 +22,94 @@ var applySourceMap = require('vinyl-sourcemaps-apply');
 var convert = require('convert-source-map');
 var transfer = require('multi-stage-sourcemap').transfer;
 
-function mergeSourceMap(incomingSourceMap, outgoingSourceMap) {
-    if (typeof outgoingSourceMap === 'string' || outgoingSourceMap instanceof String) {
-        outgoingSourceMap = JSON.parse(outgoingSourceMap);
-    }
-    if (!incomingSourceMap) {
-        return outgoingSourceMap;
-    }
-    return JSON.parse(transfer({fromSourceMap: outgoingSourceMap, toSourceMap: incomingSourceMap}));
+function mergeSourceMap (incomingSourceMap, outgoingSourceMap) {
+  if (typeof outgoingSourceMap === 'string' || outgoingSourceMap instanceof String) {
+    outgoingSourceMap = JSON.parse(outgoingSourceMap);
+  }
+  if (!incomingSourceMap) {
+    return outgoingSourceMap;
+  }
+  return JSON.parse(transfer({ fromSourceMap: outgoingSourceMap, toSourceMap: incomingSourceMap }));
 }
 
 function overwritePropertyIfExists (name, from, to) {
-    if (from.hasOwnProperty(name)) {
-        to.setProperty(name, from[name]);
-    }
+  if (Object.prototype.hasOwnProperty.call(from, name)) {
+    to.setProperty(name, from[name]);
+  }
 }
 
 function applyUnassertWithSourceMap (file, encoding, opt) {
-    var inMap = file.sourceMap;
-    var code = file.contents.toString(encoding);
+  var inMap = file.sourceMap;
+  var code = file.contents.toString(encoding);
 
-    var ast = acorn.parse(code, { ecmaVersion: 2018, sourceType: 'module', locations: true });
-    var instrumented = escodegen.generate(unassert(ast), {
-        file: file.relative,
-        sourceMap: file.relative,
-        sourceMapWithCode: true
-    });
-    var outMap = convert.fromJSON(instrumented.map.toString());
-    overwritePropertyIfExists('sources', inMap, outMap);
-    overwritePropertyIfExists('sourcesContent', inMap, outMap);
-    overwritePropertyIfExists('sourceRoot', inMap, outMap);
+  var ast = acorn.parse(code, { ecmaVersion: 2018, sourceType: 'module', locations: true });
+  var instrumented = escodegen.generate(unassert(ast), {
+    file: file.relative,
+    sourceMap: file.relative,
+    sourceMapWithCode: true
+  });
+  var outMap = convert.fromJSON(instrumented.map.toString());
+  overwritePropertyIfExists('sources', inMap, outMap);
+  overwritePropertyIfExists('sourcesContent', inMap, outMap);
+  overwritePropertyIfExists('sourceRoot', inMap, outMap);
 
-    var reMap;
-    if (inMap.mappings === '') {
-        // when incoming SourceMap is an initial sourceMap created by gulp-sourcemaps
-        applySourceMap(file, outMap.toJSON());
-        reMap = convert.fromObject(file.sourceMap);
-    } else {
-        reMap = convert.fromObject(mergeSourceMap(inMap, outMap.toJSON()));
-    }
-    overwritePropertyIfExists('sources', inMap, reMap);
-    overwritePropertyIfExists('sourcesContent', inMap, reMap);
-    overwritePropertyIfExists('sourceRoot', inMap, reMap);
-    overwritePropertyIfExists('file', inMap, reMap);
+  var reMap;
+  if (inMap.mappings === '') {
+    // when incoming SourceMap is an initial sourceMap created by gulp-sourcemaps
+    applySourceMap(file, outMap.toJSON());
+    reMap = convert.fromObject(file.sourceMap);
+  } else {
+    reMap = convert.fromObject(mergeSourceMap(inMap, outMap.toJSON()));
+  }
+  overwritePropertyIfExists('sources', inMap, reMap);
+  overwritePropertyIfExists('sourcesContent', inMap, reMap);
+  overwritePropertyIfExists('sourceRoot', inMap, reMap);
+  overwritePropertyIfExists('file', inMap, reMap);
 
-    file.contents = bufferFrom(instrumented.code);
-    file.sourceMap = reMap.toObject();
+  file.contents = bufferFrom(instrumented.code);
+  file.sourceMap = reMap.toObject();
 }
 
 function applyUnassertWithoutSourceMap (code) {
-    var ast = acorn.parse(code, { ecmaVersion: 2018, sourceType: 'module' });
-    return escodegen.generate(unassert(ast));
+  var ast = acorn.parse(code, { ecmaVersion: 2018, sourceType: 'module' });
+  return escodegen.generate(unassert(ast));
 }
 
 function transform (file, encoding, opt) {
-    if (file.sourceMap) {
-        applyUnassertWithSourceMap(file, encoding, opt);
-    } else {
-        file.contents = bufferFrom(applyUnassertWithoutSourceMap(file.contents.toString(encoding)));
-    }
+  if (file.sourceMap) {
+    applyUnassertWithSourceMap(file, encoding, opt);
+  } else {
+    file.contents = bufferFrom(applyUnassertWithoutSourceMap(file.contents.toString(encoding)));
+  }
 }
 
 module.exports = function (opt) {
-    return through.obj(function (file, encoding, callback) {
-        encoding = encoding || 'utf8';
-        if (file.isNull()) {
-            this.push(file);
-        } else if (file.isBuffer()) {
-            try {
-                transform(file, encoding, opt);
-                this.push(file);
-            } catch (err) {
-                return callback(new PluginError('gulp-unassert', err, {showStack: true}));
-            }
-        } else if (file.isStream()) {
-            file.contents = file.contents.pipe(new BufferStreams(function(err, buf, cb) {
-                if(err) {
-                    cb(new PluginError('gulp-unassert', err, {showStack: true}));
-                } else {
-                    try {
-                        var modifiedCode = applyUnassertWithoutSourceMap(buf.toString(encoding));
-                        cb(null, bufferFrom(modifiedCode));
-                    } catch (innerError) {
-                        return callback(new PluginError('gulp-unassert', innerError, {showStack: true}));
-                    }
-                }
-            }));
-            this.push(file);
+  return through.obj(function (file, encoding, callback) {
+    encoding = encoding || 'utf8';
+    if (file.isNull()) {
+      this.push(file);
+    } else if (file.isBuffer()) {
+      try {
+        transform(file, encoding, opt);
+        this.push(file);
+      } catch (err) {
+        return callback(new PluginError('gulp-unassert', err, { showStack: true }));
+      }
+    } else if (file.isStream()) {
+      file.contents = file.contents.pipe(new BufferStreams(function (err, buf, cb) {
+        if (err) {
+          cb(new PluginError('gulp-unassert', err, { showStack: true }));
+        } else {
+          try {
+            var modifiedCode = applyUnassertWithoutSourceMap(buf.toString(encoding));
+            cb(null, bufferFrom(modifiedCode));
+          } catch (innerError) {
+            return callback(new PluginError('gulp-unassert', innerError, { showStack: true }));
+          }
         }
-        callback();
-    });
+      }));
+      this.push(file);
+    }
+    callback();
+  });
 };
--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-/*global describe, it*/
+/* global describe, it */
 'use strict';
 
 delete require.cache[require.resolve('../')];
@@ -9,223 +9,221 @@ var Vinyl = require('vinyl');
 var unassert = require('../');
 
 describe('gulp-unassert', function () {
-
-    it('ES2018 syntax', function (done) {
-        var stream = unassert();
-        var srcStream = new Vinyl({
-            path: process.cwd() + "/test/fixtures/es2018/fixture.js",
-            cwd: process.cwd(),
-            base: process.cwd() + "/test/fixtures/es2018",
-            contents: fs.createReadStream('test/fixtures/es2018/fixture.js')
-        });
-        var expectedFile = new Vinyl({
-            path: process.cwd() + '/test/fixtures/es2018/expected.js',
-            cwd: process.cwd(),
-            base: process.cwd() + '/test/fixtures/es2018',
-            contents: fs.readFileSync('test/fixtures/es2018/expected.js')
-        });
-        stream.on('error', function(err) {
-            assert(!err.message);
-            done();
-        });
-        stream.on('data', function (newFile) {
-            assert(newFile);
-            assert(newFile.contents);
-            newFile.contents.pipe(es.wait(function(err, data) {
-                assert(!err);
-                assert.equal(data.toString('utf-8') + '\n', String(expectedFile.contents));
-                done();
-            }));
-        });
-        stream.write(srcStream);
-        stream.end();
+  it('ES2018 syntax', function (done) {
+    var stream = unassert();
+    var srcStream = new Vinyl({
+      path: process.cwd() + '/test/fixtures/es2018/fixture.js',
+      cwd: process.cwd(),
+      base: process.cwd() + '/test/fixtures/es2018',
+      contents: fs.createReadStream('test/fixtures/es2018/fixture.js')
     });
+    var expectedFile = new Vinyl({
+      path: process.cwd() + '/test/fixtures/es2018/expected.js',
+      cwd: process.cwd(),
+      base: process.cwd() + '/test/fixtures/es2018',
+      contents: fs.readFileSync('test/fixtures/es2018/expected.js')
+    });
+    stream.on('error', function (err) {
+      assert(!err.message);
+      done();
+    });
+    stream.on('data', function (newFile) {
+      assert(newFile);
+      assert(newFile.contents);
+      newFile.contents.pipe(es.wait(function (err, data) {
+        assert(!err);
+        assert.equal(data.toString('utf-8') + '\n', String(expectedFile.contents));
+        done();
+      }));
+    });
+    stream.write(srcStream);
+    stream.end();
+  });
 
-    
-    it('should produce expected file via buffer', function (done) {
-        var stream = unassert();
-        var srcFile = new Vinyl({
-            path: process.cwd() + "/test/fixtures/patterns/fixture.js",
-            cwd: process.cwd(),
-            base: process.cwd() + "/test/fixtures/patterns",
-            contents: fs.readFileSync('test/fixtures/patterns/fixture.js')
-        });
-        var expectedFile = new Vinyl({
-            path: process.cwd() + '/test/fixtures/patterns/expected.js',
-            cwd: process.cwd(),
-            base: process.cwd() + '/test/fixtures/patterns',
-            contents: fs.readFileSync('test/fixtures/patterns/expected.js')
-        });
-        stream.on('error', function(err) {
-            assert(err);
-            done(err);
-        });
-        stream.on('data', function (newFile) {
-            assert(newFile);
-            assert(newFile.contents);
-            assert.equal(String(newFile.contents), String(expectedFile.contents));
-            done();
+  it('should produce expected file via buffer', function (done) {
+    var stream = unassert();
+    var srcFile = new Vinyl({
+      path: process.cwd() + '/test/fixtures/patterns/fixture.js',
+      cwd: process.cwd(),
+      base: process.cwd() + '/test/fixtures/patterns',
+      contents: fs.readFileSync('test/fixtures/patterns/fixture.js')
+    });
+    var expectedFile = new Vinyl({
+      path: process.cwd() + '/test/fixtures/patterns/expected.js',
+      cwd: process.cwd(),
+      base: process.cwd() + '/test/fixtures/patterns',
+      contents: fs.readFileSync('test/fixtures/patterns/expected.js')
+    });
+    stream.on('error', function (err) {
+      assert(err);
+      done(err);
+    });
+    stream.on('data', function (newFile) {
+      assert(newFile);
+      assert(newFile.contents);
+      assert.equal(String(newFile.contents), String(expectedFile.contents));
+      done();
+    });
+    stream.write(srcFile);
+    stream.end();
+  });
+
+  it('should produce expected file via stream', function (done) {
+    var stream = unassert();
+    var srcStream = new Vinyl({
+      path: process.cwd() + '/test/fixtures/patterns/fixture.js',
+      cwd: process.cwd(),
+      base: process.cwd() + '/test/fixtures/patterns',
+      contents: fs.createReadStream('test/fixtures/patterns/fixture.js')
+    });
+    var expectedFile = new Vinyl({
+      path: process.cwd() + '/test/fixtures/patterns/expected.js',
+      cwd: process.cwd(),
+      base: process.cwd() + '/test/fixtures/patterns',
+      contents: fs.readFileSync('test/fixtures/patterns/expected.js')
+    });
+    stream.on('error', function (err) {
+      assert(err);
+      done();
+    });
+    stream.on('data', function (newFile) {
+      assert(newFile);
+      assert(newFile.contents);
+      newFile.contents.pipe(es.wait(function (err, data) {
+        assert(!err);
+        assert.equal(data.toString('utf-8'), String(expectedFile.contents));
+        done();
+      }));
+    });
+    stream.write(srcStream);
+    stream.end();
+  });
+
+  describe('should not throw but emit error when the file has a syntax error', function () {
+    it('when file is Buffer', function (done) {
+      var stream = unassert();
+      var srcFile = new Vinyl({
+        path: process.cwd() + '/test/fixtures/syntax-error/fixture.js',
+        cwd: process.cwd(),
+        base: process.cwd() + '/test/fixtures/syntax-error',
+        contents: fs.readFileSync('test/fixtures/syntax-error/fixture.js')
+      });
+      assert.doesNotThrow(function () {
+        stream.on('error', function (err) {
+          assert(err);
+          done();
         });
         stream.write(srcFile);
         stream.end();
+      });
     });
-
-    it('should produce expected file via stream', function (done) {
-        var stream = unassert();
-        var srcStream = new Vinyl({
-            path: process.cwd() + "/test/fixtures/patterns/fixture.js",
-            cwd: process.cwd(),
-            base: process.cwd() + "/test/fixtures/patterns",
-            contents: fs.createReadStream('test/fixtures/patterns/fixture.js')
-        });
-        var expectedFile = new Vinyl({
-            path: process.cwd() + '/test/fixtures/patterns/expected.js',
-            cwd: process.cwd(),
-            base: process.cwd() + '/test/fixtures/patterns',
-            contents: fs.readFileSync('test/fixtures/patterns/expected.js')
-        });
-        stream.on('error', function(err) {
-            assert(err);
-            done();
-        });
-        stream.on('data', function (newFile) {
-            assert(newFile);
-            assert(newFile.contents);
-            newFile.contents.pipe(es.wait(function(err, data) {
-                assert(!err);
-                assert.equal(data.toString('utf-8'), String(expectedFile.contents));
-                done();
-            }));
+    it('when file is Stream', function (done) {
+      var stream = unassert();
+      var srcStream = new Vinyl({
+        path: process.cwd() + '/test/fixtures/syntax-error/fixture.js',
+        cwd: process.cwd(),
+        base: process.cwd() + '/test/fixtures/syntax-error',
+        contents: fs.createReadStream('test/fixtures/syntax-error/fixture.js')
+      });
+      assert.doesNotThrow(function () {
+        stream.on('error', function (err) {
+          assert(err);
+          done();
         });
         stream.write(srcStream);
         stream.end();
+      });
+    });
+  });
+
+  describe('SourceMap support', function () {
+    it('with initial sourceMap created by gulp-sourcemaps', function (done) {
+      var stream = unassert();
+      var srcFileContents = fs.readFileSync('test/fixtures/patterns/fixture.js');
+      var srcFile = new Vinyl({
+        path: process.cwd() + '/test/fixtures/patterns/fixture.js',
+        cwd: process.cwd(),
+        base: process.cwd() + '/test/fixtures/patterns',
+        contents: srcFileContents
+      });
+      // simulate initial raw map created by gulp-sourcemaps
+      srcFile.sourceMap = {
+        version: 3,
+        file: srcFile.relative,
+        names: [],
+        mappings: '',
+        sources: [srcFile.relative],
+        sourcesContent: [srcFileContents.toString('utf8')]
+      };
+      stream.on('error', function (err) {
+        assert(err);
+        done(err);
+      });
+      stream.on('data', function (newFile) {
+        assert(newFile);
+        assert(newFile.contents);
+        assert.equal(newFile.contents.toString(), fs.readFileSync('test/fixtures/patterns/expected.js', 'utf8'));
+        assert(newFile.sourceMap, 'push file.sourceMap to downstream');
+        assert.deepEqual(newFile.sourceMap, {
+          version: 3,
+          sources: [
+            'fixture.js'
+          ],
+          names: [
+            'add',
+            'a',
+            'b'
+          ],
+          mappings: 'AAAA;AAIA,SAASA,GAAT,CAAcC,CAAd,EAAiBC,CAAjB,EAAoB;AAAA,IAqDhB,OAAOD,CAAA,GAAIC,CAAX,CArDgB;AAAA',
+          file: 'fixture.js',
+          sourcesContent: [srcFileContents.toString('utf8')]
+        });
+        done();
+      });
+      stream.write(srcFile);
+      stream.end();
     });
 
-    describe('should not throw but emit error when the file has a syntax error', function () {
-        it('when file is Buffer', function (done) {
-            var stream = unassert();
-            var srcFile = new Vinyl({
-                path: process.cwd() + "/test/fixtures/syntax-error/fixture.js",
-                cwd: process.cwd(),
-                base: process.cwd() + "/test/fixtures/syntax-error",
-                contents: fs.readFileSync("test/fixtures/syntax-error/fixture.js")
-            });
-            assert.doesNotThrow(function() {
-                stream.on("error", function(err) {
-                    assert(err);
-                    done();
-                });
-                stream.write(srcFile);
-                stream.end();
-            });
+    it('with upstream sourceMap', function (done) {
+      var stream = unassert();
+      var originalFileContents = fs.readFileSync('test/fixtures/coffee/fixture.coffee', 'utf8');
+      var srcFile = new Vinyl({
+        path: process.cwd() + '/test/fixtures/coffee/fixture.js',
+        cwd: process.cwd(),
+        base: process.cwd() + '/test/fixtures/coffee',
+        contents: fs.readFileSync('test/fixtures/coffee/fixture.js')
+      });
+      srcFile.sourceMap = {
+        version: 3,
+        sources: ['fixture.coffee'],
+        names: [],
+        mappings: 'AAAA,IAAA;;AAAA,MAAA,GAAS,OAAA,CAAQ,QAAR;;AAET,GAAA,GAAM,SAAC,CAAD,EAAI,CAAJ;EACJ,OAAO,CAAC,MAAR,CAAe,OAAO,CAAP,KAAY,QAA3B;EACA,MAAA,CAAO,CAAC,KAAA,CAAM,CAAN,CAAR;EACA,MAAM,CAAC,KAAP,CAAa,OAAO,CAApB,EAAuB,QAAvB;EACA,MAAM,CAAC,EAAP,CAAU,CAAC,KAAA,CAAM,CAAN,CAAX;SACA,CAAA,GAAI;AALA',
+        file: 'fixture.js',
+        sourceRoot: '',
+        sourcesContent: [originalFileContents]
+      };
+      stream.on('error', function (err) {
+        assert(err);
+        done(err);
+      });
+      stream.on('data', function (newFile) {
+        assert(newFile);
+        assert(newFile.contents);
+        assert.equal(newFile.contents.toString() + '\n', fs.readFileSync('test/fixtures/coffee/expected.js', 'utf8'));
+        assert(newFile.sourceMap, 'push file.sourceMap to downstream');
+        assert.deepEqual(newFile.sourceMap, {
+          version: 3,
+          sources: ['fixture.coffee'],
+          names: [],
+          mappings: 'AAAA,IAAA,GAAA,EAAA,MAAA;AAEA,GAAA,GAAM,UAAC,CAAD,EAAI,CAAJ,EAAA;AAAA,WAKJ,CAAA,GAAI,EALA;AAAA,CAAN',
+          file: 'fixture.js',
+          sourceRoot: '',
+          sourcesContent: [originalFileContents]
         });
-        it('when file is Stream', function (done) {
-            var stream = unassert();
-            var srcStream = new Vinyl({
-                path: process.cwd() + "/test/fixtures/syntax-error/fixture.js",
-                cwd: process.cwd(),
-                base: process.cwd() + "/test/fixtures/syntax-error",
-                contents: fs.createReadStream("test/fixtures/syntax-error/fixture.js")
-            });
-            assert.doesNotThrow(function() {
-                stream.on("error", function(err) {
-                    assert(err);
-                    done();
-                });
-                stream.write(srcStream);
-                stream.end();
-            });
-        });
+        done();
+      });
+      stream.write(srcFile);
+      stream.end();
     });
-
-    describe('SourceMap support', function () {
-        it('with initial sourceMap created by gulp-sourcemaps', function (done) {
-            var stream = unassert();
-            var srcFileContents = fs.readFileSync('test/fixtures/patterns/fixture.js');
-            var srcFile = new Vinyl({
-                path: process.cwd() + "/test/fixtures/patterns/fixture.js",
-                cwd: process.cwd(),
-                base: process.cwd() + "/test/fixtures/patterns",
-                contents: srcFileContents
-            });
-            // simulate initial raw map created by gulp-sourcemaps
-            srcFile.sourceMap = {
-                version : 3,
-                file: srcFile.relative,
-                names: [],
-                mappings: '',
-                sources: [ srcFile.relative ],
-                sourcesContent: [ srcFileContents.toString('utf8') ]
-            };
-            stream.on("error", function(err) {
-                assert(err);
-                done(err);
-            });
-            stream.on("data", function (newFile) {
-                assert(newFile);
-                assert(newFile.contents);
-                assert.equal(newFile.contents.toString(), fs.readFileSync('test/fixtures/patterns/expected.js', 'utf8'));
-                assert(newFile.sourceMap, 'push file.sourceMap to downstream');
-                assert.deepEqual(newFile.sourceMap, {
-                    version: 3,
-                    sources: [
-                        'fixture.js'
-                    ],
-                    names: [
-                        'add',
-                        'a',
-                        'b'
-                    ],
-                    mappings: 'AAAA;AAIA,SAASA,GAAT,CAAcC,CAAd,EAAiBC,CAAjB,EAAoB;AAAA,IAqDhB,OAAOD,CAAA,GAAIC,CAAX,CArDgB;AAAA',
-                    file: 'fixture.js',
-                    sourcesContent: [ srcFileContents.toString('utf8') ] 
-                });
-                done();
-            });
-            stream.write(srcFile);
-            stream.end();
-        });
-
-        it('with upstream sourceMap', function (done) {
-            var stream = unassert();
-            var originalFileContents = fs.readFileSync('test/fixtures/coffee/fixture.coffee', 'utf8');
-            var srcFile = new Vinyl({
-                path: process.cwd() + "/test/fixtures/coffee/fixture.js",
-                cwd: process.cwd(),
-                base: process.cwd() + "/test/fixtures/coffee",
-                contents: fs.readFileSync('test/fixtures/coffee/fixture.js')
-            });
-            srcFile.sourceMap = {
-                version: 3,
-                sources: [ 'fixture.coffee' ],
-                names: [],
-                mappings: 'AAAA,IAAA;;AAAA,MAAA,GAAS,OAAA,CAAQ,QAAR;;AAET,GAAA,GAAM,SAAC,CAAD,EAAI,CAAJ;EACJ,OAAO,CAAC,MAAR,CAAe,OAAO,CAAP,KAAY,QAA3B;EACA,MAAA,CAAO,CAAC,KAAA,CAAM,CAAN,CAAR;EACA,MAAM,CAAC,KAAP,CAAa,OAAO,CAApB,EAAuB,QAAvB;EACA,MAAM,CAAC,EAAP,CAAU,CAAC,KAAA,CAAM,CAAN,CAAX;SACA,CAAA,GAAI;AALA',
-                file: 'fixture.js',
-                sourceRoot: '',
-                sourcesContent: [ originalFileContents ]
-            };
-            stream.on("error", function(err) {
-                assert(err);
-                done(err);
-            });
-            stream.on("data", function (newFile) {
-                assert(newFile);
-                assert(newFile.contents);
-                assert.equal(newFile.contents.toString() + '\n', fs.readFileSync('test/fixtures/coffee/expected.js', 'utf8'));
-                assert(newFile.sourceMap, 'push file.sourceMap to downstream');
-                assert.deepEqual(newFile.sourceMap, {
-                    version: 3,
-                    sources: [ 'fixture.coffee' ],
-                    names: [],
-                    mappings: 'AAAA,IAAA,GAAA,EAAA,MAAA;AAEA,GAAA,GAAM,UAAC,CAAD,EAAI,CAAJ,EAAA;AAAA,WAKJ,CAAA,GAAI,EALA;AAAA,CAAN',
-                    file: 'fixture.js',
-                    sourceRoot: '',
-                    sourcesContent: [ originalFileContents ] 
-                });
-                done();
-            });
-            stream.write(srcFile);
-            stream.end();
-        });
-    });
+  });
 });
